2584b7402aa1f0b2cde6e80c0ad55656af9eb57d
cangjie2020 <74097429+cangjie2020@users.noreply.github.com>
2024-10-29 16:48:46 +0800

    Create length.py

---
+++ script II/length.py
@@ -0,0 +1,13 @@
+import argparse
+import numpy as np
+parser = argparse.ArgumentParser(description='计算片段长度信息')
+parser.add_argument('-gene_info_file', help='输入转录组比对的Sam文件')
+args = parser.parse_args()
+ff = open(args.gene_info_file)                 #之前产生的sam文件
+num=[]
+for line in ff :
+    line1 = line.split('\t')
+    length=len(line1[9])                       #碱基序列长度
+num.append(int(length))                        #吧碱基序列长度数据加入列表
+length_mean=np.mean(num)                       #计算平均值
+print(length_mean)
